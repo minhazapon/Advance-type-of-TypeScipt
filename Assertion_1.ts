daed2d59c2f97c54ccc6e9d3459c92e380fe351e
minhazapon <minhajulabedinapon@gmail.com>
2024-11-01 09:14:21 -0700

    assertion file

---
+++ Assertion_1.ts
@@ -0,0 +1,31 @@
+{
+
+
+    let foodName1 = 'biriyani';
+    ( foodName1 as string )
+    console.log(foodName1)
+
+
+    let num = 12345;
+    let name = 'apon';
+    let country = 'bangladesh';
+    let id = 10111;
+    let name1 = true;
+
+
+    (num as number);
+    (name as string);
+    (country as string);
+    (id as number);
+    (name1 as boolean)
+
+
+    console.log(num)
+    console.log(name)
+    console.log(country)
+    console.log(id)
+    console.log(name1)
+    
+
+
+}
--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@
     console.log(name)
    
 
-
+    
 
 
 }
