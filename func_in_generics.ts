53df2b51d6c428d8673f7610fc75e97cc4e8308d
minhazapon <minhajulabedinapon@gmail.com>
2024-11-02 06:03:21 -0700

    added F generic file

---
+++ func_in_generics.ts
@@ -0,0 +1,21 @@
+{
+
+      
+
+     const math = <T> ( math1: T, math2: T, math3: T, math4: T ) =>{
+
+         
+        const allMath = { math1, math2, math3, math4 }
+
+        return allMath
+        
+
+     }
+
+     const allM = math<number>(12, 234, 24, 2)
+
+     console.log(allM)
+
+
+
+}
